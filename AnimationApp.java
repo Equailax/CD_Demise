8e51de44c696d36a182415685c765e5b0006f62f
Amman Yusuf <amman.yusuf@ucalgary.ca>
2019-02-12 15:16:30 -0700

    Created the Animation App

---
+++ AnimationApp.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+
+public class AnimationApp{
+    /*
+    Interact with the human player to ask it for a move.
+    Move the avatar as specified
+    Check if any obstacles are hit.  If so react appropriately.
+    Check if any collectibles have been reached.  If so, react appropriately.
+    Move any collectibles that are movable.
+    */
+    
+    
+    //Instance variables
+    private Avatar minidisc;
+    private ArrayList<Collectibles>;
+    private ArrayList<Obstacle>;
+    
+    
+    //Methods
+    
+
+}
