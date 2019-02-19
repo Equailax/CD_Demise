3f31e631df9511aa8588fa0ddf343b959f56e113
Amman Yusuf <amman.yusuf@ucalgary.ca>
2019-02-18 20:47:28 -0700

    initialization methods are added, still missing location constructors for avatar,collectible,and obstacles ----> have to have x and y int coordinates, not a rectangle class as part of the constructor

--- AnimationApp.java
+++ AnimationApp.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.Scanner;
+import java.lang.Math;
 
 
 public class AnimationApp{
@@ -50,14 +51,18 @@ public class AnimationApp{
     
     //Instance variables
     private Avatar minidisc;
-    private ArrayList<Collectible> collectibesArray;
+    private ArrayList<Collectible> collectiblesArray;
     private ArrayList<Obstacle> obstacleArray;
     
     //Constructors
     /**
     The default constructor for the AnimationApp class has the instance variables set to their default values
     */
-    public AnimationApp(){}
+    public AnimationApp(){
+        this.minidisc = new Avatar();
+        this.collectiblesArray = new ArrayList<Collectible>();
+        this.obstacleArray = new ArrayList<Obstacle>();
+    }
     
     /**
     This constructor allows to initialize and set the intance variables to a prefered value
@@ -67,22 +72,22 @@ public class AnimationApp{
         
         //Copy the input avatar and set the newAvatar equal to the instance variable
         if (inputAvatar != null){
-            this.Avatar = new Avatar(inputAvatar);
+            this.minidisc = new Avatar(inputAvatar);
         }else {
-            this.Avatar = null;
+            this.minidisc = null;
         }
         
         //Copy the inputCollectiblesArray and set it equal to the instance variable
         if (inputCollectiblesArray != null){
             ArrayList<Collectible> tempCollectiblesArrayList = new ArrayList<Collectible>();
-            for (Collectible c : this.collectibesArray){
+            for (Collectible c : this.collectiblesArray){
                 if (c != null){
                     tempCollectiblesArrayList.add(new Collectible(c));
                 }else {
                     tempCollectiblesArrayList.add(new Collectible());
                 }
             }
-            this.collectibesArray = tempCollectiblesArrayList;
+            this.collectiblesArray = tempCollectiblesArrayList;
         }
         
         //Copy the inputObstaclesArray and set it equal to the instance variable
@@ -105,25 +110,25 @@ public class AnimationApp{
     @param inputAvatar : this is the avatar we wish to set this.avatar to
     */
     public void setAvatar(Avatar inputAvatar){
-        this.Avatar = new Avatar(inputAvatar);
+        this.minidisc = new Avatar(inputAvatar);
     }
     
     /** 
-    This method sets this.collectibesArray to a new array 
-    @param inputCollectiblesArray : the collectibesArray that we wish to set this.collectibesArray to
+    This method sets this.collectiblesArray to a new array 
+    @param inputCollectiblesArray : the collectiblesArray that we wish to set this.collectiblesArray to
     */
     public void setCollectiblesArray(ArrayList<Collectible> inputCollectiblesArray){
         ArrayList<Collectible> tempCollectiblesArrayList = new ArrayList<Collectible>();
         if (inputCollectiblesArray != null){
             ArrayList<Collectible> tempCollectiblesArrayList = new ArrayList<Collectible>();
-            for (Collectible c : this.collectibesArray){
+            for (Collectible c : this.collectiblesArray){
                 if (c != null){
                     tempCollectiblesArrayList.add(new Collectible(c));
                 }else {
                     tempCollectiblesArrayList.add(new Collectible());
                 }
             }
-            this.collectibesArray = tempCollectiblesArrayList;
+            this.collectiblesArray = tempCollectiblesArrayList;
         }
     }
     
@@ -163,21 +168,86 @@ public class AnimationApp{
     }
     
     /**
-    This method will add a collectible to the array at the end of the collectibesArray 
-    @param collectibleToAdd : the collectible we wish to add to the array
+    This method will add a specified number of obstacles to the end of the obstacleArray
+    @param numberOfObstaclesToAdd : this is the number of (new) obstacles we wish to add
     */
-    public void addCollectible(Collectible collectibleToAdd){
-        this.collectibesArray.add(new Collectible(collectibleToAdd));
+    public void addObstacle(int numberOfObstaclesToAdd){
+        ArrayList tempObstaclesArrayList = new ArrayList<Obstacle>();
+        
+        //Copy the obsacle array list if it is not empty
+        if (this.obstacleArray != null){
+            for (Obstacle o : this.obstacleArray){
+                if (o != null){
+                    tempObstaclesArrayList.add(new Obstacle(o));
+                }else {
+                    tempObstaclesArrayList.add(new Obstacle());
+                }
+            }
+        }
+        
+        //Add the required obstacles to the temporary array list
+        for (int i = 0; i < numberOfObstaclesToAdd; i++){
+            
+            //Generate a random number between 0 and 10 for the x coordinate
+            int randomXCoordinate = (int)(Math.random() * 10 + 0);
+            
+            //Generate a random number between 0 and 10 for the Y coordinate
+            int randomYCoordinate = (int)(Math.random() * 10 + 0);
+            
+            tempObstaclesArrayList.add(new Obstacle("Obstacle" + i, randomXCoordinate, randomYCoordinate))  ///When constructing an obstacle, it should take a random positon as an argument
+        }
+        this.obstacleArray = tempObstaclesArrayList;
     }
     
     /**
-    This method allows to add a collectible at a speicfic index of the collectibesArray
+    This method will add a collectible to the array at the end of the collectiblesArray 
+    @param collectibleToAdd : the collectible we wish to add to the array
+    */
+    public void addCollectible(Collectible collectibleToAdd){
+        this.collectiblesArray.add(new Collectible(collectibleToAdd));
+    }
+    
+    /**
+    This method allows to add a collectible at a speicfic index of the collectiblesArray
     @param index : the index (which is an integer) of the array in which we wish to add the collectible
     @param collectibleToAdd : the collectible we wish to add to the array
     */
      public void addCollectible(int index, Collectible collectibleToAdd){
-        this.collectibesArray.add(index, new Collectible(collectibleToAdd));
+        this.collectiblesArray.add(index, new Collectible(collectibleToAdd));
     }
+    
+    /**
+    This method will adda specified number of collectibles to the collectibles array list
+    @param numberOfCollectiblesToAdd : this is the number of collectibles we wish to add to the array list
+    */
+    public void addCollectible(int numberOfCollectiblesToAdd){
+        ArrayList<Collectible> tempCollectiblesArrayList = new ArrayList<Collectible>();
+        
+        //Copy the collectibles array list if it is not empty
+        if (this.collectiblesArray != null){
+            for (Collectible c : this.collectiblesArray){
+                if (c != null){
+                    tempCollectiblesArrayList.add(new Collectible(c));
+                }else {
+                    tempCollectiblesArrayList.add(new Collectible());
+                }
+            }
+        }
+        
+        //Add the required number of collecitbles that one wished to add
+        for (int i = 0; i < numberOfCollectiblesToAdd; i++){
+            
+            //Generate a random number between 0 and 10 for the x coordinate
+            int randomXCoordinate = (int)(Math.random() * 10 + 0);
+            
+            //Generate a random number between 0 and 10 for the Y coordinate
+            int randomYCoordinate = (int)(Math.random() * 10 + 0);
+            
+            tempCollectiblesArrayList.add(new Collectible("Collectible" + i, randomXCoordinate, randomYCoordinate))  ///When constructing a collectible, it should take a random positon as an argument
+        }
+        this.collectiblesArray = tempCollectiblesArrayList;
+    }
+    
     
     /**
     This will print the current state of the game.  This means avatar position/health, collectibes and obstacles
@@ -191,15 +261,90 @@ public class AnimationApp{
     }
     
     public void processAvatarMove(){
-    
+        
     }
     
+    /**
+    This method initializes the game.  What that means is it creates the obstacles and collecitbles for the stage
+    and places the avatar at the starting position. 
+    */
     public void initialize(){
-    
+        //Initalize the avatar
+        this.avatar.setPosition() ///\/\/\/\/\/\/\/\/\/\/ IMPORTANT we want to make sure tha the origianl position of  the avatar is at the starting points
+        
+        //Initialize Collectibles -- Add 5 collectibles -- make sure the positions of collectibles are correct
+        addCollectible(5)
+        
+        //Initalize Obstacles -- Add 5 obstacles -- make sure the postions of the obstacles are correct
+        addObstacle(5);  
     }
+    
+    /**This method initializes the game while the game runs.  This means keeping the positons of the 'original' objects that
+    where created but also adding obstacles and collecitbles accordingly
+    @param collecitblesToAdd : this is the number of new collecitbles that the game should add after completing a certain task
+    @param obstaclesToAdd : this is the number of new obsatacles that the game shoudl add.
+    */
+    public void initialize(int collecitblesToAdd, int obstacleToAdd){
+        
+        //Add any collectibles if necessary
+        addCollectible(collecitblesToAdd);
+        
+        //Add any obstacles if necessary
+        addObstacle(obstacleToAdd);
+    }
+    
     
     //Main Run 
     public static void main(String[] args){
+        int movements = 0;
+        
+        AnimationApp mainApp = new AnimationApp();
+        
+        //Initialize the game (obtacle, collectible, and avatar positions)
+        mainApp.initialize();
+        
+        //While the number of allowed movements is less than the required ammount (10), play a turn of the game
+            //Might add condition of 'winning' where they collect all 5 collectibles
+        while (movements < 10){
+        
+        //Prompt the user for a movement
+        System.out.print("Move UP, DOWN, LEFT, RIGHT:");
+		Scanner userMovmentInput = new Scanner(System.in);
+        
+        //This chunk of code processes if the avatar can move\\
+        //Create a copy avatar
+            //Move the avatar accordingly 
+            //Check if the move is valid by checking the obstacle and collecitbles array
+                //Collect collectible if its there
+                    //Move the original avatar accordingly (processAvatarMove())
+                    //Remove the collectible from the map
+                    
+                //Take damage from the obstacle if its there
+                    //Dont move the original avatar accoringly (processAvatarMove())
+                
+                //If its at the edge of the world, its an improper move and should be done nothing
+                    //Dont move the avatar (processAvatarMove())
+                
+                //If nothing is there
+                    //Move the original avatar accoringly (processAvatarMove())
+        
+        //Move obstacle accordingly (make sure to not overlap with avatar)
+            //Update positions of all obstacles in arraylist
+        
+        //Print the current state
+        
+        //Draw the current state
+        
+        //Check if end condition is met (number of tuns lets say)
+        }
         
     }
 }
+
+
+
+
+
+
+
+
