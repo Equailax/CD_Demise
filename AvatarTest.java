dd8a564baf0e551292a587c869503dc44ed8e799
Abel Tekeste <abeltekeste3@gmail.com>
2019-03-24 21:09:56 -0600

    added test files

---
+++ AvatarTest.java
@@ -0,0 +1,131 @@
+import static org.junit.Assert.*;
+import java.beans.Transient;
+import org.junit.Test;
+import java.awt.Rectangle;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.paint.ImagePattern;
+
+
+public class AvatarTest {
+
+    public class MockAvatar extends Avatar{
+        public MockAvatar(){super();};
+        public MockAvatar(String myName, int myLife, int myhealth, int myDamage, Rectangle location){
+            super(myName, myLife, myhealth, myDamage, location);
+        }
+        public MockAvatar(Avatar avatarToCopy){
+            super(avatarToCopy);
+        }
+
+    }
+
+    @Test
+    public void test_Instance_Variables(){
+        Rectangle location = new Rectangle(100, 100, 54, 67);
+        Avatar avatar = new MockAvatar("Avatar", 4, 4, 1, location);
+        assertEquals("Expected name 'Avatar'.", "Avatar", avatar.getName());
+        assertEquals("Expected 4 health.", 4, avatar.getHealth(), 0.00001);
+        assertEquals("Expected 4 lives.", 4, avatar.getLives(), 0.00001);
+        assertEquals("Expected 1 damage.", 1, avatar.getDamage(), 0.00001);
+        assertEquals("Expected Xposition to be 100.", 100, avatar.getLocation().getX(), 0.00001);
+        assertEquals("Expected Yposition to be 100.", 100, avatar.getLocation().getY(), 0.00001);
+        assertEquals("Expected width to be 100.", 54, avatar.getLocation().getWidth(), 0.00001);
+        assertEquals("Expected height to be 100.", 67, avatar.getLocation().getHeight(), 0.00001);
+    }
+
+    @Test
+    public void test_Image_Instance_Variables(){
+        Avatar avatar = new MockAvatar();
+        assertEquals("Expected Xposition to be 100.", 100, avatar.getXImageLayout(), 0.00001);
+        assertEquals("Expected Yposition to be 100.", 100, avatar.getYImageLayout(), 0.00001);
+    
+    }
+
+    @Test
+    public void test_copy_constructor(){
+        Rectangle location = new Rectangle(100, 100, 54, 67);
+        Avatar toCopy = new Avatar("Avatar", 4, 4, 1, location);
+        Avatar avatar = new MockAvatar(toCopy);
+        assertEquals("Expected name to be Avatar.", toCopy.getName(), avatar.getName());
+        assertEquals("Expected health to be 4.", toCopy.getHealth(), avatar.getHealth(), 0.00001);
+        assertEquals("Expected lives to be 4.", toCopy.getLives(), avatar.getLives(), 0.00001);
+        assertEquals("Expected damage to be 1.", toCopy.getDamage(), avatar.getDamage(), 0.00001);
+        assertEquals("Expected Xposition to be 100.", toCopy.getLocation().getX(), avatar.getLocation().getX(), 0.00001);
+        assertEquals("Expected Yposition to be 100.", toCopy.getLocation().getY(), avatar.getLocation().getY(), 0.00001);
+        assertEquals("Expected width to be 100.", toCopy.getLocation().getWidth(), avatar.getLocation().getWidth(), 0.00001);
+        assertEquals("Expected height to be 100.", toCopy.getLocation().getHeight(), avatar.getLocation().getHeight(), 0.00001);
+    }
+
+    @Test 
+    public void test_take_damage(){
+        Rectangle location = new Rectangle(100, 100, 54, 67);
+        Avatar avatar = new MockAvatar("Avatar", 4, 4, 1, location);
+        avatar.takeDamage(2);
+        assertEquals("Expected health to be 2.", 2, avatar.getHealth(), 0.00001);
+    }
+
+    @Test 
+    public void test_gain_health(){
+        Rectangle location = new Rectangle(100, 100, 54, 67);
+        Avatar avatar = new MockAvatar("Avatar", 4, 4, 1, location);
+        avatar.gainHealth(2);
+        assertEquals("Expected health to be 2.", 6, avatar.getHealth(), 0.00001);
+    }
+
+    @Test
+    public void test_movement() {
+        Rectangle location = new Rectangle(100, 100, 54, 67);
+        Avatar avatar = new MockAvatar("Avatar", 4, 4, 1, location);
+        avatar.move("Down");
+        assertEquals("Expected YPosition to be 3.", 3, avatar.getLocation().getY(), 0.00001);
+        avatar.move("UP");
+        assertEquals("Expected YPosition to be 0.", 0, avatar.getLocation().getY(), 0.00001);
+        avatar.move("RIGHT");
+        assertEquals("Expected XPosition to be 3.", 3, avatar.getLocation().getX(), 0.00001);
+        avatar.move("LEFT");
+        assertEquals("Expected XPosition to be 0.", 0, avatar.getLocation().getX(), 0.00001);
+    }
+
+    @Test 
+    public void test_toString(){
+        Rectangle location = new Rectangle(100, 100, 54, 67);
+        Avatar avatar = new MockAvatar("Avatar", 4, 4, 1, location);
+        assertEquals("Excpeted toString to gives same string", "Position: 100 100 Health: 4 Lives: 4", avatar.toString());
+    }
+
+    @Test
+    public void test_endgame(){
+        Rectangle location = new Rectangle(100, 100, 54, 67);
+        Avatar avatar = new MockAvatar("Avatar", 0, 0, 1, location);
+        avatar.checkIfEndGame(0);
+        assertEquals("Excpeted to return true if 0 lives and 0 health", true, avatar.checkIfEndGame(0));
+        }
+    
+
+    @Test 
+    public void test_locations_when_copied(){
+        Rectangle location = new Rectangle(100, 100, 54, 67);
+        Avatar toCopy = new Avatar("Avatar", 4, 4, 1, location);
+        Avatar avatar = new MockAvatar(toCopy);
+        assertEquals("Excpeted to return true if location are the same", true, avatar.equals(toCopy));
+    }
+
+    @Test 
+    public void test_setter_methods(){
+        Avatar avatar = new MockAvatar();
+        avatar.setName("Avatar");
+        avatar.setHealth(4);
+        avatar.setLives(4);
+        avatar.setLocation(100, 100);
+        avatar.setDamage(1);
+        assertEquals("Expected name 'Avatar'.", "Avatar", avatar.getName());
+        assertEquals("Expected 4 health.", 4, avatar.getHealth(), 0.00001);
+        assertEquals("Expected 4 lives.", 4, avatar.getLives(), 0.00001);
+        assertEquals("Expected 1 damage.", 1, avatar.getDamage(), 0.00001);
+        assertEquals("Expected Xposition to be 100.", 100, avatar.getLocation().getX(), 0.00001);
+        assertEquals("Expected Yposition to be 100.", 100, avatar.getLocation().getY(), 0.00001);
+        assertEquals("Expected width to be 100.", 54, avatar.getLocation().getWidth(), 0.00001);
+        assertEquals("Expected height to be 100.", 67, avatar.getLocation().getHeight(), 0.00001);
+    }
+}
---
+++ CollectibleTest.java
@@ -0,0 +1,66 @@
+import static org.junit.Assert.*;
+import java.beans.Transient;
+import org.junit.Test;
+import java.awt.Rectangle;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.paint.ImagePattern;
+
+public class CollectibleTest{
+    public class MockCollectible extends Collectible{
+        public MockCollectible(){super();}
+        public MockCollectible(Collectible toCopy){super(toCopy);}
+        public MockCollectible(String name, int xcoord, int ycoord){super(name, xcoord, ycoord);}
+    }
+
+    @Test 
+    public void test_creation(){
+        Collectible a = new MockCollectible("Collectible", 100, 100);
+        assertEquals("Excpected name to be 'Collectible'", "Collectible", a.getName());
+        assertEquals("Excpected Xcoord to be 100", 100, a.getLocation().getX());
+        assertEquals("Excpected Ycoord to be 100", 100, a.getLocation().getY());
+    }
+
+    @Test 
+    public void test_copy_constructor(){
+        Collectible toCopy = new MockCollectible("Collectible", 100, 100);
+        Collectible collectible = new MockCollectible(toCopy);
+        assertEquals("Excpected name to be 'Collectible'", toCopy.getName(), collectible.getName());
+        assertEquals("Excpected Xcoord to be 100", toCopy.getLocation().getX(), collectible.getLocation().getX());
+        assertEquals("Excpected Ycoord to be 100", toCopy.getLocation().getY(), collectible.getLocation().getY());
+    }
+
+    @Test 
+    public void test_setter_methods(){
+        Collectible collectible = new MockCollectible();
+        collectible.setName("New Name");
+        collectible.setLocation(100, 100);
+        assertEquals("Excpected name to be 'New Name'", "New Name", collectible.getName());
+        assertEquals("Excpected Xcoord to be 100", 100, collectible.getLocation().getX());
+        assertEquals("Excpected Ycoord to be 100", 100, collectible.getLocation().getY());
+    }
+
+    @Test 
+    public void test_overlaps(){
+        Avatar a = new Avatar();
+        a.setLocation(100, 100);
+        Collectible collectible = new MockCollectible("Collectible", 100, 100);
+        assertEquals("Expected to return true if overlapping", true, collectible.overlapsWith(a));
+    }
+
+    @Test 
+    public void test_toString(){
+        Collectible collectible = new MockCollectible("Collectible", 100, 100);
+        assertEquals("Expected right toString()", "Collectible 100 100", collectible.toString());
+    }
+
+    @Test 
+    public void test_add_to_collection(){
+        Collectible collectible = new MockCollectible("Collectible", 100, 100);
+        collectible.addToCollection();
+        collectible.addToCollection();
+        assertEquals("Excpected Collectiong to be 2", 2, collectible.getCollection());
+    }
+
+    
+}
