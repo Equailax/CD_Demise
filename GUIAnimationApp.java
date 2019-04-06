cb0f9d1d19d1deebc0c99f5df204e9511969da55
Richard Hoang <richard.hoang1@ucalgary.ca>
2019-04-06 13:08:07 -0600

    GUIAnimationApp formatting

--- GUIAnimationApp.java
+++ GUIAnimationApp.java
@@ -54,18 +54,17 @@ public class GUIAnimationApp extends Application {
     private Pane root = new Pane();
     private final Scene scene = new Scene(root, 1000, 1000, new ImagePattern(map));
     
-    //Instance variables
-    //This creades an animation app instance
+    // Instance variables
+    // This creades an animation app instance
     private AnimationApp demo2 = new AnimationApp();
     
-    //These are the image arrays for enemies and collectibles
+    // These are the image arrays for enemies and collectibles
     private ArrayList<Obstacle> obstacleGUIArray = demo2.getObstacleArray();
     private ArrayList<Collectible> collectibleGUIArray = demo2.getCollectiblesArray();
     
     private Avatar mini = demo2.getAvatar();
     
-    
-    
+
     /**
     This is the initialize method for the gui animation application
     */
@@ -76,7 +75,6 @@ public class GUIAnimationApp extends Application {
         collectibleGUIArray = demo2.getCollectiblesArray();
         mini = demo2.getAvatar();
         
-        
         // Display positions of life hearts for health
         Text life = new Text(5, 90, "LIVES: " + demo2.getAvatar().getLives());
         life.setFont(new Font("Times New Roman", 15));
@@ -84,7 +82,7 @@ public class GUIAnimationApp extends Application {
         textList.add(life);
         root.getChildren().add(life);
         
-        //Display the health as a number
+        // Display Health Number
         Text health = new Text(5, 120, "HEALTH\n      " + demo2.getAvatar().getHealth());
         
         health.setFont(new Font("Times New Roman", 15));
@@ -92,14 +90,12 @@ public class GUIAnimationApp extends Application {
         textList.add(health);
         root.getChildren().add(health);
         
-        //Distplay the obstacles
+        // Distplay Obstacles
         for (Obstacle o : this.obstacleGUIArray) {
             root.getChildren().add(o.getEnemyImageRectangle());
         }
         
-        
         // Display Collectibles
-        
         for(Collectible c : this.collectibleGUIArray){
             c.setImage();
             root.getChildren().add(c.getCollectibleImageRectangle());
@@ -111,13 +107,11 @@ public class GUIAnimationApp extends Application {
         collection.setFill(Color.YELLOW);
         textList.add(collection);
         root.getChildren().add(collection);
-
     }
-
-    public static void main(String[] args) {
-        
-        launch(args); // launches GUI application
-
+	
+	// Launches GUI application
+    public static void main(String[] args) { 
+        launch(args); 
     }
 
     public void start(Stage primaryStage) throws Exception {
@@ -125,41 +119,32 @@ public class GUIAnimationApp extends Application {
         initialize();
           
         // Rectangle / hitbox
-		Rectangle mouseHitbox = new Rectangle(0, 0, 1250, 1250);           // IMPORTANT: Make the rectangle fill the whole window
+		Rectangle mouseHitbox = new Rectangle(0, 0, 1250, 1250); // IMPORTANT: Make the rectangle fill the whole window
 		mouseHitbox.setFill(Color.rgb(0,0,0,0));
-        
-        //root.getChildren().add(mini.getAvatarImage());
-        
+		root.getChildren().add(mouseHitbox);
         root.getChildren().add(mini.getAvatarImage());
 		mini.setForward();
-        
-        root.getChildren().add(mouseHitbox);
-		
 		mouseHitbox.toFront();
         primaryStage.setScene(scene);
         primaryStage.setResizable(false);
         primaryStage.show();
 		
-        
-
-        //Animation of movements
+        // Animation of movements
         AnimationTimer moveTime = new AnimationTimer(){
             @Override
             public void handle(long now) {
                 int moveX = 0;
                 int moveY = 0;
                 
-                
-                
-                //Avatar updatedAvatar = new Avatar(demo2.getAvatar());
+                // Avatar updatedAvatar = new Avatar(demo2.getAvatar());
                 Avatar avatarBeforeMovement = new Avatar(demo2.getAvatar());
                 if (right) {
                     mini.setForward();
                     
-                    //Check if moving right is valid by the ANIMATIONAPP LOGIC
+                    // Check if moving right is valid by the ANIMATIONAPP LOGIC
                     demo2.processAvatarMove("right");
                     
-                    //if the avatar's movement changed, set the moveX to + 3, if not, nothing has changed
+                    // If the avatar's movement changed, set the moveX to +5, if not, nothing has changed
                     if (avatarBeforeMovement.equals(demo2.getAvatar())){
                         moveX -= 0;
                     } else {
@@ -169,10 +154,10 @@ public class GUIAnimationApp extends Application {
                 if (left) {
                     mini.setBackward();
                     
-                    //Check if moving right is valid by the ANIMATIONAPP LOGIC
+                    // Check if moving left is valid by the ANIMATIONAPP LOGIC
                     demo2.processAvatarMove("left");
                     
-                    //if the avatar's movement changed, set the moveX to + 3, if not, nothing has changed
+                    // If the avatar's movement changed, set the moveX to -5, if not, nothing has changed
                     if (avatarBeforeMovement.equals(demo2.getAvatar())){
                         moveX -= 0;
                     } else {
@@ -181,10 +166,10 @@ public class GUIAnimationApp extends Application {
                 }
 				avatarBeforeMovement = new Avatar(demo2.getAvatar());
                 if (up) {
-                    //Check if moving right is valid by the ANIMATIONAPP LOGIC
+                    // Check if moving up is valid by the ANIMATIONAPP LOGIC
                     demo2.processAvatarMove("up");
                     
-                    //if the avatar's movement changed, set the moveX to + 3, if not, nothing has changed
+                    // If the avatar's movement changed, set the moveY to -5, if not, nothing has changed
                     if (avatarBeforeMovement.equals(demo2.getAvatar())){
                         moveY-= 0;
                     } else {
@@ -192,10 +177,10 @@ public class GUIAnimationApp extends Application {
                     }
                 }
                 if (down) {
-                    //Check if moving right is valid by the ANIMATIONAPP LOGIC
+                    // Check if moving down is valid by the ANIMATIONAPP LOGIC
                     demo2.processAvatarMove("down");
                     
-                    //if the avatar's movement changed, set the moveX to + 3, if not, nothing has changed
+                    // If the avatar's movement changed, set the moveY to +5, if not, nothing has changed
                     if (avatarBeforeMovement.equals(demo2.getAvatar())){
                         moveY -= 0;
                     } else {
@@ -203,17 +188,17 @@ public class GUIAnimationApp extends Application {
                     }
                 }
                 
-                //Check if the player has run out of health
+                // Check if the player has run out of health
                 Avatar checkIfEndGameAvatar = new Avatar(demo2.getAvatar());
                 if (demo2.getCollectiblesArray().size() == 0) {
-                    //If all collectibles have reached 0, the game is over
+                    // If all collectibles have reached 0, the game is over
                     root.getChildren().clear();
                     root.getChildren().add(new Label("YOU WIN!"));
                 } else if (!checkIfEndGameAvatar.checkIfEndGame(3)){
-                    //If the avatar still has lives (game hasnt ended), then update the main avatar accordingly
+                    // If the avatar still has lives (game hasn't ended), then update the main avatar accordingly
                     demo2.setAvatar(checkIfEndGameAvatar);
                     
-                    //Clear collectible count and health count
+                    // Clear collectible count and health count
                     for (Text t : textList) {
                         root.getChildren().remove(t);
                         
@@ -233,35 +218,28 @@ public class GUIAnimationApp extends Application {
                         }
                     }
                     
-                    //Process the obstacle movement remove anything if necessary
+                    // Process the obstacle movement remove anything if necessary
                     demo2.processObstacleMove();
                     demo2.removeObstacles();
-                    
+                    // Enemies
                     for (Obstacle o : obstacleGUIArray) {
                         root.getChildren().remove(o.getEnemyImageRectangle());
                     }
-                    
+                    // Obstacles
                     obstacleGUIArray = demo2.getObstacleArray();
-                    
                     for (Obstacle o : obstacleGUIArray) {
                         root.getChildren().add(o.getEnemyImageRectangle());
                     }
-                    
-                    mouseHitbox.toFront();
-                    
-                    //Update collectibles
+					
+                    // Collectibles
                     for (Collectible c : collectibleGUIArray) {
                         root.getChildren().remove(c.getCollectibleImageRectangle());
                     }
-                    
                     collectibleGUIArray = demo2.getCollectiblesArray();
-                    
                     for (Collectible c : collectibleGUIArray) {
                         c.setImage();
                         root.getChildren().add(c.getCollectibleImageRectangle());
                     }
-                    
-                    mouseHitbox.toFront();
                     
                     if (((mini.getXImageLayout() + moveX) <= 772) && ((mini.getXImageLayout() + moveX) >= 0)) {
                         if(((mini.getYImageLayout() + moveY) >= 0) && ((mini.getYImageLayout() + moveY) <= 575)) {
@@ -269,14 +247,11 @@ public class GUIAnimationApp extends Application {
                             root.getChildren().remove(mini.getAvatarImage());
                             
                             mini.moveAvatarImage(moveX, moveY);
-
                             mini.setLocation((int)mini.getXImageLayout() + 100, (int)mini.getYImageLayout() + 100);
-                            
                             mini.setHealth(demo2.getAvatar().getHealth());
                             mini.setLives(demo2.getAvatar().getLives());
-                            
                             demo2.setAvatar(mini);
-                            
+							
                             root.getChildren().add(mini.getAvatarImage());
                         
                         }
@@ -284,176 +259,117 @@ public class GUIAnimationApp extends Application {
                     mouseHitbox.toFront();
                     
                 }else if (checkIfEndGameAvatar.checkIfEndGame(3)){
-                    //If the end game has been reached (avatar has lost all of their health and lives) then break from the loop and end the game
+                    // If the end game has been reached (avatar has lost all of their health and lives) then break from the loop and end the game
                     root.getChildren().clear();
                     root.getChildren().add(new Label("Game over man"));
                 }
-
             }};
             
-        // Movement Key Events
+        // Move when a key is pressed
         scene.setOnKeyPressed(keyEvent -> {
-                // Starts moving right when key is pressed
                 if (keyEvent.getCode().toString() == "D")
                     right = true;
-        
-                // Starts moving left when key is pressed
                 if (keyEvent.getCode().toString() == "A")
                     left = true;
-        
-                // Starts moving up when key is pressed
                 if (keyEvent.getCode().toString() == "W")
                     up = true;
-        
-                // Starts moving down when key is pressed
                 if (keyEvent.getCode().toString() == "S")
                     down = true;
         });
 
+		// Stop moving when the key is no longer pressed
         scene.setOnKeyReleased(keyEvent -> {
-                // Stops moving right when key is released
                 if (keyEvent.getCode().toString() == "D")
                     right = false;
-        
-                // Stops moving left when key is released
                 if (keyEvent.getCode().toString() == "A")
                     left = false;
-        
-                // Stops moving up when key is released
                 if (keyEvent.getCode().toString() == "W")
                     up = false;
-        
-                // Stops moving down when key is released
                 if (keyEvent.getCode().toString() == "S")
                     down = false;
         });
 		
+		// When clicking left mouse button
 		mouseHitbox.setOnMouseClicked(mouseEvent ->
 		{
 			// Calculate the center of the avatar relative to the window
 			double avatarXCenter = (demo2.getAvatar().getLocation().getX()+27);
 			double avatarYCenter = (demo2.getAvatar().getLocation().getY()+33.5);
-			System.out.println(demo2.getAvatar().getLocation()); // CAN BE DELETED
-            
+			
+			// Print game state
+			System.out.println(demo2.getAvatar().getLocation());
             System.out.println(mini.getXImageLayout() + " " + mini.getYImageLayout());
-            
             System.out.println(mini.getAvatarImage().getX() + " " + mini.getAvatarImage().getY());
-            
             demo2.printCurrentState();
             
-			// Calculate the anglet between the avatar and the mouse
-
+			// Calculate the angle between the avatar and the mouse
 			double angle = Math.toDegrees(Math.atan2(mouseEvent.getX()-(avatarXCenter), mouseEvent.getY()-(avatarYCenter)))+180;
-			System.out.println("Click (" + mouseEvent.getX() + ", " + mouseEvent.getY() + ")"); // CAN BE DELETED
-			System.out.println(angle);// CAN BE DELETED
+			System.out.println("Click (" + mouseEvent.getX() + ", " + mouseEvent.getY() + ")");
+			System.out.println(angle);
 			
 			// Print to console the direction that the mouse click is relative to the avatar's rectangle.
 			// The angle is a circle with 0 starting at the top and increases as the mouse moves counter clockwise
+			// Shoot a projectile in the direction clicked
 			if (angle > 337.5)
 			{
 				System.out.println("North");
-                
                 mini.shootProjectile("up");
-                
                 Projectile avatarProjectile = new Projectile(mini.getProjectile());
-                
-                this.obstacleGUIArray.add(avatarProjectile);
-                demo2.setObstacleArray(this.obstacleGUIArray);
-                
-                
+				this.obstacleGUIArray.add(avatarProjectile);
 			} else if (angle > 292.5)
 			{
 				System.out.println("North East");
-                
                 mini.shootProjectile("upright");
-                
                 Projectile avatarProjectile = new Projectile(mini.getProjectile());
-                
-                this.obstacleGUIArray.add(avatarProjectile);
-                demo2.setObstacleArray(this.obstacleGUIArray);
-                
+				this.obstacleGUIArray.add(avatarProjectile);
 			} else if (angle > 247.5)
 			{
 				System.out.println("East");
-                
                 mini.shootProjectile("right");
-                
                 Projectile avatarProjectile = new Projectile(mini.getProjectile());
-                
-                this.obstacleGUIArray.add(avatarProjectile);
-                demo2.setObstacleArray(this.obstacleGUIArray);
-                
+				this.obstacleGUIArray.add(avatarProjectile);
 			} else if (angle > 202.5)
 			{
 				System.out.println("South East");
-                
                 mini.shootProjectile("downright");
-                
                 Projectile avatarProjectile = new Projectile(mini.getProjectile());
-                
-                this.obstacleGUIArray.add(avatarProjectile);
-                demo2.setObstacleArray(this.obstacleGUIArray);
-                
+				this.obstacleGUIArray.add(avatarProjectile);
 			} else if (angle > 157.5)
 			{
 				System.out.println("South");
-                
                 mini.shootProjectile("down");
-                
                 Projectile avatarProjectile = new Projectile(mini.getProjectile());
-                
-                this.obstacleGUIArray.add(avatarProjectile);
-                demo2.setObstacleArray(this.obstacleGUIArray);
-                
+				this.obstacleGUIArray.add(avatarProjectile);
 			} else if (angle > 112.5)
 			{
 				System.out.println("South West");
-                
                 mini.shootProjectile("downleft");
-                
                 Projectile avatarProjectile = new Projectile(mini.getProjectile());
-                
-                this.obstacleGUIArray.add(avatarProjectile);
-                demo2.setObstacleArray(this.obstacleGUIArray);
-                
+				this.obstacleGUIArray.add(avatarProjectile);
 			} else if (angle > 67.5)
 			{
 				System.out.println("West");
-                
                 mini.shootProjectile("left");
-                
                 Projectile avatarProjectile = new Projectile(mini.getProjectile());
-                
-                this.obstacleGUIArray.add(avatarProjectile);
-                demo2.setObstacleArray(this.obstacleGUIArray);
-                
+				this.obstacleGUIArray.add(avatarProjectile);
 			} else if (angle > 22.5)
 			{
 				System.out.println("North West");
-                
                 mini.shootProjectile("upleft");
-                
                 Projectile avatarProjectile = new Projectile(mini.getProjectile());
-                
-                this.obstacleGUIArray.add(avatarProjectile);
-                demo2.setObstacleArray(this.obstacleGUIArray);
-                
+				this.obstacleGUIArray.add(avatarProjectile);
 			} else if (angle >= 0)
 			{
 				System.out.println("North");
-                
                 mini.shootProjectile("up");
-                
                 Projectile avatarProjectile = new Projectile(mini.getProjectile());
-                
-                this.obstacleGUIArray.add(avatarProjectile);
-                demo2.setObstacleArray(this.obstacleGUIArray);
-                
+				this.obstacleGUIArray.add(avatarProjectile);
 			}
-			
+            demo2.setObstacleArray(this.obstacleGUIArray);
 			mouseHitbox.toFront();
 		});
 		
+		// Enemy Attack Timer
 		Timeline enemyTimer = new Timeline(new KeyFrame(Duration.seconds(2.5), ActionEvent ->
 		{
 			ArrayList<Obstacle> newProjectile = new ArrayList<Obstacle>();
@@ -464,23 +380,19 @@ public class GUIAnimationApp extends Application {
 					((Enemy)o).shootProjectile("left");
 					Projectile enemyProjectile = new Projectile(((Enemy)o).getProjectile());
 					newProjectile.add(enemyProjectile);
-					
 				}
 			}
 			this.obstacleGUIArray.addAll(newProjectile);
 			demo2.setObstacleArray(this.obstacleGUIArray);
 		}));
-		
-		
-        
+
+		// Start timers
 		enemyTimer.setCycleCount(Timeline.INDEFINITE);
 		enemyTimer.play();
-		
-
 		moveTime.start();
 		
+		// Print game state
         demo2.printCurrentState();
-
     }
 };
 
